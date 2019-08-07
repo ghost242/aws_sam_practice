4176083283cca25c842c277aa8eb171398c56c2d
aroh <aroh@madup.com>
2019-08-07 13:39:48 +0900

    Add dockerfile for aws deploy environment based in docker

--- app_stack/hello_lambda/app.py
+++ app_stack/hello_lambda/app.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import boto3
@@ -5,25 +6,32 @@ import boto3
 
 def lambda_handler(event, context):
     try:
+        print('Enter in LambdaHandler!!')
         bucket_name = os.environ.get('DATASTORE_BUCKETNAME')
         s3_client = boto3.client('s3')
 
         for record in event['Records']:
             print("test")
             payload = record["body"]
-            print(str(payload))
+            print(str(payload), type(payload), repr(payload))
 
-            s3_client.meta.client.upload_fileobj(payload.encode('utf-8'),
-                                                 bucket_name,
-                                                 'test_file.txt')
+            s3_client.put()
+            res = s3_client.upload_file(text,
+                                        bucket_name,
+                                        'test_file.txt')
+            print(res)
     except Exception as e:
         code = 500
         msg = str(e)
+        print("Error!! {}".format(msg))
     else:
         code = 200
         msg = 'Success'
+        print("Success!! {}".format(msg))
     finally:
         return {
-            'code': code,
-            'message': msg
+            "statusCode": code,
+            "body": json.dumps({
+                "message": msg
+            }),
         }
