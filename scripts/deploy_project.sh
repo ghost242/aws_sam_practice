c107503c006b613b90cd58b4cf428e43d82392cd
aroh <aroh@madup.com>
2019-08-09 01:44:21 +0900

    build: add-on CI tools
    add travis.yaml
    update test codes for updated source codes.
    not sure
    correctly run this code.

--- scripts/deploy_project.sh
+++ scripts/deploy_project.sh
@@ -1,1 +1,2 @@
+cd ..
 sam deploy --template-file ./.aws-sam/build/packaged_template.yaml --capabilities CAPABILITY_IAM --stack-name HelloWorld3
--- scripts/package_project.sh
+++ scripts/package_project.sh
@@ -1,1 +1,2 @@
+cd ..
 sam package --template-file .aws-sam/build/template.yaml --s3-bucket sam-practice-lambda-src --output-template-file ./.aws-sam/build/packaged_template.yaml
